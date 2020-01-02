a4565b94e3f99890d9e256cf98cb01715ca67ab6
erisanderos <59073587+erisanderos@users.noreply.github.com>
2020-01-02 13:16:55 -0700

    new version: vUpdate

--- ArraySetup.java
+++ ArraySetup.java
@@ -1,8 +1,24 @@
 package sample;
 
+import java.util.Arrays;
+
 public class ArraySetup {
 //ArraySetup is meant to be a class that gives the user the ability to manipulate data from an excel spreadsheet
-//once ExcelHandling()
+    private String[][] mainArray;
 
+    public String[][] setArray(String excelFP){
+        String[][] nArray = new ExcelHandling().readExcel(excelFP);
+        //call makeExcel with nArray to save to a new file
+        this.mainArray = nArray;
+        System.out.println(Arrays.deepToString(nArray));
+        return nArray;
+    }
 
+    public static void sendArray(String excelFP, String[][] nArray){
+        new ExcelHandling().makeExcel(excelFP, nArray);
+    }
+
+    public String[][] getArray(){
+        return this.mainArray;
+    }
 }
